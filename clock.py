39a3d2d68f35bf57ae84fbb27bfca3f19cba1249
taranosovivan <vanataranosov@gmail.com>
2026-05-22 10:06:33 +0400

    Add files via upload
    my first project

---
+++ clock.py
@@ -0,0 +1,17 @@
+# from tkinter import *
+# from time import strftime
+#
+# root = Tk()
+# root.title('Цифровые часы')
+#
+# label = Label(root, font=('aerial', 30), background='black', foreground='white')
+# label.pack(anchor='center')
+#
+# def time():
+#     string = strftime('%H:%M:%S %p')
+#     label.config(text=string)
+#     label.after(1000, time)
+#
+# time()
+# mainloop()
+
